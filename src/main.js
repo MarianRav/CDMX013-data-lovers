8c0f3a023902bdb6f84b4ed15da5dbcb5cc80e90
Marian Villavicencio <marian.mlva.16@gmail.com>
2022-08-08 18:19:03 -0500

    Función para traer info

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+console.log(data.results.name[0]);
+
+const container = document.getElementById('container');
+
+data.results.forEach(element =>{
+    container.innerHTML=`
+    <div class="name">Name: ${data.results.name}
+                 ${data.results.species}
+                 ${data.results.status}</div>`
+})
