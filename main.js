eeb21bb20688c4563f418be67ad4539edf60a174
Marian Villavicencio <marian.mlva.16@gmail.com>
2022-08-10 19:36:28 -0500

    Deploy final

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/rickandmorty/rickandmorty.js';
+import data from './rickandmorty/rickandmorty.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
