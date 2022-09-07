3739ce6c33fb355759a1fd7efc15d9e0acc470ee
Marian Villavicencio <marian.mlva.16@gmail.com>
2022-09-06 19:07:44 -0500

    Updates

--- data.js
+++ data.js
@@ -1,6 +1,39 @@
-export const filterGender = (genero, data)=>  data.filter(character => character.gender == genero);
-export const filterSpecies= (especie, data) => data.filter(character => character.species== especie);
-export const filterStatus= (status, data) => data.filter(character => character.status== status);
-export const filterType= (tipo, data) => data.filter(character => character.type== tipo);
-export const filterOrigin= (origen, data) => data.filter(character => character.origin.name== origen);
-export const filterLocation= (ubicacion, data) => data.filter(character => character.location.name== ubicacion);
+export const filterGender = (genero, data) => data.filter(character => character.gender == genero);
+export const filterSpecies = (especie, data) => data.filter(character => character.species == especie);
+export const filterStatus = (status, data) => data.filter(character => character.status == status);
+export const filterType = (tipo, data) => data.filter(character => character.type == tipo);
+export const filterOrigin = (origen, data) => data.filter(character => character.origin.name == origen);
+export const filterLocation = (ubicacion, data) => data.filter(character => character.location.name == ubicacion);
+
+export const alphaOrd = (data) => {
+  return data.sort((a, b) => {
+     if (a.name.toUpperCase() > b.name.toUpperCase()) {
+       return -1;
+     }
+     if (a.name.toUpperCase() < b.name.toUpperCase ()) {
+       return 1;
+     }
+   });
+ }
+ 
+export const searchCharacter =(data, input)=>{
+  let array=[];
+  data.forEach(character =>{
+   let databaseInfo = character.name.toLowerCase();
+   if(databaseInfo.indexOf(input)!== -1){
+    //if (character.status==="unknown")
+   array.push(character)
+   }
+
+  })
+  return array;
+}
+
+//Statistics %
+
+export const computeStats =(quantity, total ) => {
+  let totalPer=(quantity*100) / total;
+  console.log(typeof parseInt (totalPer.toFixed(0)));
+  return parseInt (Math.floor(totalPer)
+  );
+}
--- main.js
+++ main.js
@@ -5,38 +5,17 @@ import { filterType } from './data.js';
 import { filterOrigin } from './data.js';
 import { filterLocation } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
+import {alphaOrd} from './data.js';
+import {searchCharacter} from './data.js';
+import {computeStats} from './data.js';
 console.log(filterSpecies);
 console.log(data.results[0]);
 
 //mostrar data
 const section = document.getElementById('container'); 
-
-data.results.forEach(element =>{
-        let html= `
-        <section class="pickles class="flex-boxx"">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-           
-        </div>
-        </section>
-        `;
-       section.innerHTML+=html
-})
-
-let dataCharacters= data.results
-
-//Button to reset All Characters
-
-const allCharacters = document.getElementById('btnAllCharacters'); 
-allCharacters.addEventListener("click", resetCharacters);
-function resetCharacters () {
-  section.innerHTML="";
-  console.log(allCharacters);
-  dataCharacters.forEach(element =>{
+const sliceResult= data.results.slice(0, 100)
+function allCards (allData) {
+  allData.forEach(element =>{
     let html= `
     <section class="pickles class="flex-boxx"">
     <img src=${element.image} class="image">
@@ -51,12 +30,42 @@ function resetCharacters () {
     `;
    section.innerHTML+=html
 })
+}
+allCards(sliceResult);
+
+let dataCharacters= data.results;
+
+//Alphabetical Order
+
+const orderSelect=document.getElementById("order");
+orderSelect.addEventListener ("change", (e) => {
+  let orderBox= alphaOrd(sliceResult);
+  if(e.target.value === "Z-A"){
+    section.innerHTML="";
+    allCards(orderBox);
+  }
+  if(e.target.value === "A-Z"){
+    section.innerHTML="";
+    allCards(orderBox.reverse())
+
+  }
+
+});
+
+//Button to reset All Characters
+
+const allCharacters = document.getElementById('btnAllCharacters'); 
+allCharacters.addEventListener("click", resetCharacters);
+function resetCharacters () {
+  section.innerHTML="";
+  allCards(dataCharacters)
 gender.value="0";
 species.value="0";
 status.value="0";
 type.value="0";
 origin.value="0";
 location.value="0";
+orderSelect.value="0";
 } 
 
 
@@ -68,194 +77,233 @@ mybutton.addEventListener("click",topFunction)
    document.documentElement.scrollTop = 0;
 }
 
-
-//GENDER --opciones
-const busqueda = dataCharacters.reduce((dato, filtro)=>{
-  dato[filtro.species] = ++dato[filtro.species] || 0;
-  return dato; 
-}) 
-
-console.log(busqueda);
 //Filtrado género
 const gender = document.getElementById("gender");
 gender.addEventListener("change", (e)=>{
-     console.log(filterGender(e.target.value, dataCharacters));
     
      if (e.target.value==="0") {
       section.innerHTML=""; 
-        dataCharacters.forEach(element =>{
-       
-          let ejercicio = `
-          
-          <section class="pickles" class="flex-boxx">
-          <img src=${element.image} class="image">
-          <div class="lettersWrap" id="prueba">
-              <div class="name">  ${element.name} </div>
-              <div class="species">Species: ${element.species} </div>
-              <div class="status">Status: ${element.status}</div>
-              <img class="pin" id="logo" src= "img/pinRick&M.png">
-  
-          </div>
-          </section>`
-         section.innerHTML+=ejercicio;
-         
-        })
-       
+        allCards(sliceResult)
       }
       else {
       let typeGender = filterGender(e.target.value, dataCharacters);
       section.innerHTML="";
-      console.log(typeGender);
-      typeGender.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-       
-      })
+      allCards(typeGender)
     }  
+    
+species.value="0";
+status.value="0";
+type.value="0";
+origin.value="0";
+location.value="0";
+orderSelect.value="0";
 })
 // filter Species
 const species= document.getElementById("species");
 species.addEventListener("change", (e)=>{
-     console.log(filterSpecies(e.target.value, dataCharacters));
-  
+     
+  if (e.target.value==="0") {
+    section.innerHTML=""; 
+     allCards(sliceResult)
+    }
+    else {
       let typeSpecies = filterSpecies(e.target.value, dataCharacters);
       section.innerHTML="";
-      console.log(typeSpecies);
-      typeSpecies.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-      })
+      allCards(typeSpecies)
+    }
+    gender.value="0";
+    status.value="0";
+    type.value="0";
+    origin.value="0";
+    location.value="0";
     })
 
     // filter Status
 const status= document.getElementById("status");
 status.addEventListener("change", (e)=>{
-     console.log(filterStatus(e.target.value, dataCharacters));
-  
+  if (e.target.value==="0") {
+    section.innerHTML=""; 
+     allCards(sliceResult)
+    }
+    else {
       let typeStatus = filterStatus(e.target.value, dataCharacters);
       section.innerHTML="";
   
       console.log(typeStatus);
-      typeStatus.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-      })
+      allCards(typeStatus)
+    }
+    gender.value="0";
+    species.value="0";
+    type.value="0";
+    origin.value="0";
+    location.value="0";
     })
 
-    // filter Type
+// filter Type
 const type= document.getElementById("type");
 type.addEventListener("change", (e)=>{
-     console.log(filterType(e.target.value, dataCharacters));
-  
+  if (e.target.value==="0") {
+    section.innerHTML=""; 
+      allCards(sliceResult)
+    }
+    else {
       let type = filterType(e.target.value, dataCharacters);
       section.innerHTML="";
-     
-      console.log(type);
-      type.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-      })
+  
+      allCards(type)
+    }
+  gender.value="0";
+  species.value="0";
+  status.value="0";
+  origin.value="0";
+  location.value="0";
     })
 
-    // filter Origin
+ // filter Origin
 const origin= document.getElementById("origin");
 origin.addEventListener("change", (e)=>{
-     console.log(filterOrigin(e.target.value, dataCharacters));
-  
+
+  if (e.target.value==="0") {
+    section.innerHTML=""; 
+
+      allCards(sliceResult)
+    }
+    else {     
       let typeOrigin = filterOrigin(e.target.value, dataCharacters);
       section.innerHTML="";
-    
-      console.log(typeOrigin);
-      typeOrigin.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-      })
+      allCards(typeOrigin)
+    }
+  gender.value="0";
+  species.value="0";
+  status.value="0";
+  type.value="0";
+  location.value="0";
     })
 
-    // filter Location
+// filter Location
 const location= document.getElementById("location");
 location.addEventListener("change", (e)=>{
-     console.log(filterLocation(e.target.value, dataCharacters));
+  if (e.target.value==="0") {
+    section.innerHTML=""; 
+      allCards(sliceResult)
+    }
+    else {
   
       let typeLocation = filterLocation(e.target.value, dataCharacters);
       section.innerHTML="";
-      /*const content=document.getElementById("content")
-      content.innerHTML="";*/
-      console.log(typeLocation);
-      typeLocation.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
 
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-      })
-    })
+      allCards(typeLocation)
+    }
+  gender.value="0";
+  species.value="0";
+  status.value="0";
+  type.value="0";
+  origin.value="0";
+    });
+
+    
+    //Search Function
+let formulario=document.getElementById("inputText");
+
+formulario.addEventListener("keyup", (e) =>{
+  console.log(e.target.value);
+  let modifiedInput= e.target.value.toLowerCase();
+ if(e.target.value !== ""){
+    section.innerHTML="";
+    let searchResults= searchCharacter(dataCharacters, modifiedInput);
+    
+    console.log(searchCharacter(dataCharacters,modifiedInput));
+    if (searchResults.length===0){
+      section.innerHTML=`<div class="textNotFound"> <h2>Character not found, maybe in an unknown dimention!!</h2> </div>
+                         <div class="notFound"> <img class="portal"src= "img/rickNot.gif"></div>
+                         `
+                                         
+    } else {
+      allCards(searchResults);
+    } 
+  }else{
+    allCards(sliceResult)}});
+
+    const btnFacts= document.getElementById("funFacts");
+
+    btnFacts.addEventListener ("click", (e) => {
+      computeStats(244, 493);
+      section.innerHTML="";
+      section.innerHTML=`<section>
+                       
+                         <div  class="flex-boxx"> 
+                          
+                          <!-- Dead Morty-->
+
+                          <div class="backGrou">
+                          <div class="h3contain"> CITADEL MORNING presents FUN FACTS</div>
+                          <div class="h4contain">Another dead Morty!</div>
+                          <div class="h5contain"> Records show that from a 44 Mortys that we know until now,
+                         ${computeStats(9,44)}% are dead and ${computeStats(13,44)}% with unknown status.</div>
+                          <div> <img class="mortem" src= "img/deadmortyTV.png"></div>
+                          <div class="sarcasmMorty">What a surprise! (sarcasm) </div>
+                          </div>
+                     
+                          <!-- RICK DEAD -->
+
+                          <div class="backGrou">
+                          <div class="h3contain"> CITADEL MORNING presents FUN FACTS</div>
+                          <div class="h4containRick">Sad news today!</div>
+                          <div class="h5containRick"> Another Rick was found dead, from 73 Ricks that we know until now,
+                         ${computeStats(33,73)}% are dead and ${computeStats(19,73)}% with unknown status.</div>
+                          <div> <img class="mortemRick"src= "img/deadrickTV.png"></div>
+                          </div>
+
+                          <!-- Most populated dimention -->
+
+                          <div class="backGrou">
+                          <div class="h3contain"> CITADEL MORNING presents FUN FACTS</div>
+                          <div class="h4contain">Stop having kids!</div>
+                          <div class="h5contain">New info shows that Earth(Replacement Dimension), has the largest population of all dimensions with             
+                         ${computeStats(105,493)}%. </div>
+                          <div> <img class="mortem"src= "img/allCharTV.png"></div>
+                          <div class="noBabe">  <img class="babyRick"src= "img/babyrick.png"></div>
+                          <div class="sarcasmMorty">No more babies!</div>
+                          </div>
+
+                          <!-- HUMANOID -->
+
+                          <div class="backGrou">
+                          <div class="h3contain"> CITADEL MORNING presents FUN FACTS</div>
+                          <div class="h4containRick">It's a trap!</div>
+                          <div class="h5containRick">Experts inform that Humanoids are the species with more presence of all dimensions 
+                          after humans with a ${computeStats(53,493)}%.</div>
+                          <div> <img class="mortemRick"src= "img/humanoidTV.png"></div>
+                          </div>
+
+                          <!-- unknown origin -->
+
+                          <div class="backGrouOrigin">
+                          <div class="h3contain"> CITADEL MORNING presents FUN FACTS</div>
+                          <div class="h4contain">Don't trust anyone!</div>
+                          <div class="h5contain"> Records show that from 493 characters ${computeStats(244,493)}% are from an unkown origin.</div> 
+                          <div class="beCareful">Be careful! </div>
+                          <div> <img class="mortem"src= "img/ricksTV.png"></div>
+                          <div class="stayTuned">Stay tuned for more Funfacts here in CITADEL MORNING!</div>
+                            </div>
+                            
+                        </div>
+                            
+                        </section>
+                        `
+      console.log(e);
+     console.log(computeStats(244, 493));
+    }) 
+
+
+//Estadisticas counter.
+
+/*let count={};
+dataCharacters.forEach(item => {
+if (count[item.origin.name]) {
+    count [item.origin.name]++;
+}else {
+  count [item.origin.name]=1;
+}
+})
+console.log(count);*/
